186e7167a52811ca2edb65acb5384e5561873d2d
Omokhefue <mokhefue@gmail.com>
2023-09-21 20:08:01 +0100

    forgot password and reset password routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,11 @@
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
+
+const ErrorResponse = require("../utils/errorResponse");
+const { processImageFile } = require("../utils/imageFileUpload");
+const sendEmail = require('../utils/sendEmail');
+const crypto = require("crypto");
+
 // POST /api/auth/signup: User registration.
 exports.signup = asyncHandler(async (req, res, next) => {
   let sanitizedImageName;
@@ -16,11 +24,17 @@ exports.signup = asyncHandler(async (req, res, next) => {
     verifiedStatus: false,
   });
 
-  await user.getVerifyEmailOTP(user.email, res);
+  await user.getVerifyEmailOTP(user.email, next);
 
-  sendTokenResponse(user, 200, res);
+  if (!user.verifyEmailOTP) {
+    message =
+      "user signed up. please request an otp be sent to you to verify email";
+  } else {
+    message = "user signed up, please check email sent to you and verify email";
+  }
+
+  sendTokenResponse(user, 200, res, message);
 });
-
 // POST /api/auth/login: User login.
 exports.login = asyncHandler(async (req, res, next) => {
   const { email, password } = req.body;
@@ -28,7 +42,6 @@ exports.login = asyncHandler(async (req, res, next) => {
 
   sendTokenResponse(user, 200, res);
 });
-
 // POST /api/auth/verify-email verify email of registered user by otp
 exports.verifyEmail = asyncHandler(async (req, res, next) => {
   const { email, userEnteredOTP } = req.body;
@@ -38,29 +51,109 @@ exports.verifyEmail = asyncHandler(async (req, res, next) => {
     verifyEmailOTP: userEnteredOTP,
     verifyEmailOTPExpire: { $gt: Date.now() },
   });
+
   if (!user) {
     // User not found or OTP data is missing
     return next(new ErrorResponse("Invalid Token", 400));
   }
 
-  await user.verifyEmail(user, userEnteredOTP, res);
+  await user.verifyEmail(user, res);
 });
 // POST /api/auth/resend-otp: incase of an error when sending the first otp to verify email
 exports.resendVerifyEmailOTP = asyncHandler(async (req, res, next) => {
   const user = req.user;
+  console.log(user);
+  if (!user) {
+    return next(
+      new ErrorResponse(`not authenticated to make a new otp request`, 403)
+    );
+  }
   await user.getVerifyEmailOTP(user.email);
-  res.status(200).json({ sucess: "true", message: "new otp has been sent" });
+
+  res.status(200).json({
+    sucess: "true",
+    message: `new otp has been sent to ${user.email}`,
+  });
 });
 exports.logout = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ message: "Logout successful" });
+  // set the token to null so user has to login to receive another token
+  res.status(200).json({ message: "Logout successful", token: null });
 });
-const sendTokenResponse = async (user, statusCode, res) => {
+
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse("there is no user with that email", 404));
+  }
+
+  const resetToken = await user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  // create reset url
+  const resetURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/users/reset-password/${resetToken}`;
+
+  const message = `You are receiving this email because you need to confirm your email address. Please make a GET request to: \n\n ${resetURL}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Password reset token",
+      message,
+    }),
+      process.env.FROM_NAME,
+      process.env.FROM_EMAIL;
+  } catch (err) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+console.log(err)
+    await user.save({ validateBeforeSave: false });
+    return next(new ErrorResponse("Email could not be sent"));
+  }
+  res.status(201).json({
+    success: true,
+    message: "email sent",
+    resetToken,
+  });
+});
+
+// done
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  // get hashed token
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(req.params.resetToken)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid token", 400));
+  }
+
+  // set the new password
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
+});
+
+const sendTokenResponse = async (user, statusCode, res, message) => {
   const token = await user.getSignedJwtToken();
 
   res.status(statusCode).json({
     success: true,
     token,
     user: user._id,
+    message,
   });
 };
-
--- routes/auth.js
+++ routes/auth.js
@@ -5,16 +5,20 @@ const {
   verifyEmail,
   resendVerifyEmailOTP,
   logout,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/auth");
 
 const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/verify-email", protect, verifyEmail);
-router.post("/resend-otp", protect, resendVerifyEmailOTP);
-router.post("/logout", protect, logout);
+router.post("/signup", signup); //done
+router.post("/login", login); //done
+router.post("/verify-email", protect, verifyEmail); //done
+router.post("/resend-otp", protect, resendVerifyEmailOTP); //done
+router.post("/forgot-password", protect, forgotPassword); //test route
+router.post("/logout", protect, logout); //done
+router.put("/reset-password/:resetToken", protect, resetPassword); //test route
 
 module.exports = router;
