d3b4394c3815d3006bd6135b4aa3a51d15ff7d8d
Omokhefue <mokhefue@gmail.com>
2023-09-26 22:42:38 +0100

    add/delete Products and categories

--- utils/seeder.js
+++ utils/seeder.js
@@ -7,15 +7,25 @@ mongoose.connect(process.env.MONGO_URI);
 
 // import resource model
 const User = require("../models/User");
+const Category = require("../models/Category");
+const Product = require("../models/Product");
 // reading the file content of resource
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/../data/user.json`, "utf-8")
+);
+const categories = JSON.parse(
+  fs.readFileSync(`${__dirname}/../data/category.json`, "utf-8")
+);
+const products = JSON.parse(
+  fs.readFileSync(`${__dirname}/../data/product.json`, "utf-8")
 );
 // add files to database
 const importData = async () => {
   try {
     // create resource
     await User.create(users);
+    await Category.create(categories);
+    await Product.create(products)
     console.log("data imported");
     process.exit(1);
   } catch (error) {
@@ -28,6 +38,8 @@ const deleteData = async () => {
   try {
     // delete resource
     await User.deleteMany();
+    await Category.deleteMany();
+    await Product.deleteMany();
     console.log("data deleted");
     process.exit(1);
   } catch (error) {
