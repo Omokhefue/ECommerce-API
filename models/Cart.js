a4b392b906b73317f017bc8db83dd568c60e7eef
Omokhefue <mokhefue@gmail.com>
2023-09-26 22:40:36 +0100

    cart/wishlist model

---
+++ models/Cart.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["wishlist", "cart"],
+      required: true,
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+          min: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
