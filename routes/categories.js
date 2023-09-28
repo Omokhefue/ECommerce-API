e2b837f2210b8ca7dac8b24a64daf7d3f41fa4d8
Omokhefue <mokhefue@gmail.com>
2023-09-28 16:50:46 +0100

    updated category routes

--- routes/categories.js
+++ routes/categories.js
@@ -3,39 +3,72 @@ const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
 const { checkResourceExists } = require("../middleware/checkResourceExists");
-const { checkAuthorization } = require("../middleware/checkAuthorization");
 const {
   getCategories,
-  addCategpry,
+  addCategory,
   deleteCategory,
   updateCategory,
   getSingleCategory,
+  getParentCategory,
+  searchCategories,
 } = require("../controllers/categories");
 const { advancedResults } = require("../middleware/advancedResults");
 const Category = require("../models/Category");
 
+// Middleware for advanced results of subcategories under one category
+const subcategoriesForCategory = (req, res, next) => {
+  advancedResults(
+    Category,
+    { parentCategory: req.resource._id },
+    "subcategories"
+  )(req, res, next);
+};
+
+// Middleware for advanced results of all subcategories
+const topLevelSubcategories = (req, res, next) => {
+  advancedResults(Category, { parentCategory: { $ne: null } }, "subcategories")(
+    req,
+    res,
+    next
+  );
+};
+router.get("/search", searchCategories);
 router.get(
-  "/",
-  advancedResults(Category, null, "subcategories"),
+  "/parentcategories",
+  advancedResults(Category, { parentCategory: null }, "subcategories"),
   getCategories
+); //done
+router.get("/subcategories", topLevelSubcategories, getCategories);
+router.get(
+  "/:categorySlug",
+  checkResourceExists("category"),
+  getSingleCategory
 );
-router.get("/:CategoryId", getSingleCategory);
-router.post("/", protect, authorize("site-owner"), addCategpry);
+router.post("/", protect, authorize("site-owner"), addCategory);
 router.put(
-  "/:categoryId",
+  "/:categorySlug",
   protect,
   authorize("site-owner"),
-  checkResourceExists("Category"),
-  checkAuthorization,
+  checkResourceExists("category"),
   updateCategory
 );
 router.delete(
-  "/:categoryId",
+  "/:categorySlug",
   protect,
   authorize("site-owner"),
-  checkResourceExists("Category"),
-  checkAuthorization,
+  checkResourceExists("category"),
   deleteCategory
+);
+router.get(
+  "/:categorySlug/subcategories",
+  checkResourceExists("category"),
+  subcategoriesForCategory,
+  getCategories
+);
+router.get(
+  "/:categorySlug/category",
+  checkResourceExists("category"),
+  getParentCategory
 );
 
 module.exports = router;
