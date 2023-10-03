35fc60ed23249a004c2be6d8e3d5eb522471696b
Omokhefue <mokhefue@gmail.com>
2023-10-03 23:20:14 +0100

    Cart Model

--- models/Cart.js
+++ models/Cart.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const cartSchema = new mongoose.Schema(
   {
@@ -9,12 +10,11 @@ const cartSchema = new mongoose.Schema(
     },
     title: {
       type: String,
-      required: true,
+      enum: ["My Cart"], // Allow only "My Cart" as an option
+      default: "My Cart", // Default title if not provided
     },
-    type: {
+    slug: {
       type: String,
-      enum: ["wishlist", "cart"],
-      required: true,
     },
     items: [
       {
@@ -25,13 +25,47 @@ const cartSchema = new mongoose.Schema(
         },
         quantity: {
           type: Number,
-          default: 1,
-          min: 1,
+          default: 1, 
+          min: 1, 
         },
       },
     ],
+    totalCost: {
+      type: Number,
+    },
   },
-  { timestamps: true }
+  { timestamps: true } 
 );
 
-const Cart = mongoose.model("Cart", cartSchema);
+// Middleware executed before saving a cart document
+cartSchema.pre("save", function (next) {
+  // Generate a slug based on the title using slugify
+  this.slug = slugify(this.title, { lower: true, remove: /[*+~.()'"!:@]/g });
+
+  next(); // Continue with the save operation
+});
+
+// Calculate and update the total cost of the cart after saving
+cartSchema.post("save", async function () {
+  let totalCost = 0;
+
+  // Populate the 'items.product' field with product details and user details
+  await this.populate({
+    path: "items.product",
+    select: "user name description price image color",
+    populate: { path: "user", select: "firstname lastname" },
+  });
+
+  // Calculate the total cost by iterating through items
+  for (const item of this.items) {
+    const { product, quantity } = item;
+    if (product && product.price) {
+      totalCost += product.price * quantity;
+    }
+  }
+
+  // Update the 'totalCost' field with the calculated total cost
+  this.totalCost = totalCost.toFixed(2); // Fix the total cost to two decimal places
+});
+
+module.exports = mongoose.model("Cart", cartSchema); 
